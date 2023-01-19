b4dce5e3da9a15f359dd67bdc5f049182856660a
DiopinYT <85357474+MrCoder228re@users.noreply.github.com>
2023-01-19 21:02:09 +0500

    182

--- check_updates.bat
+++ check_updates.bat
@@ -5,10 +5,10 @@ cd %~dp0
 call :Admin
 call check_minecraft.bat
 set upt=0
-set ver=Version 1.8.1
+set ver=Version 1.8.2
 title Checking Updates...
 echo.Checking Updates...
-powershell.exe -windowstyle hidden -noprofile -c "(Invoke-WebRequest https://raw.githubusercontent.com/MrCoder228re/minecraft-patcher/main/check_updates.bat).Content" | find "%ver%" >nul && set upt=1
+curl -s "https://raw.githubusercontent.com/MrCoder228re/minecraft-patcher/main/check_updates.bat" | find "%ver%" >nul && set upt=1
 if "%upt%"=="1" (start C:\minecraft-patcher\loader\load.exe /run C:\minecraft-patcher\loader\SYSTEM.cfg)
 if "%upt%"=="0" (start https://github.com/MrCoder228re/minecraft-patcher/releases/latest)
 del chkupt.txt
